b1c327e2b98f966f3cb08f9023ac62dfe5db3aa7
Atif <atifabid859@gmail.com>
2021-03-04 12:47:20 +0530

    first commit

---
+++ swing/src/swing/JFrame.java
@@ -0,0 +1,23 @@
+package swing;
+import javax.swing.*;
+public class JFrame {
+	private String EXIT_ON_CLOSE;
+	private void setDefaultCloseOperation(String exitOnClose) {
+	}
+	
+	private void setVisible(boolean b) {
+		
+		
+	}
+public static void main(String[] args) {
+	JFrame frame=new JFrame();
+	
+	frame.setVisible(true);
+	frame.setDefaultCloseOperation(frame.EXIT_ON_CLOSE);
+	
+}
+
+
+
+
+}
